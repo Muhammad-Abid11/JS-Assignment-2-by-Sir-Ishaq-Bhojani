028821e4660d3fa271f9849ce4b53dfe46d09a20
Muhammad-Abid11 <abidkhan11845@Gmail.com>
2023-04-11 18:34:59 +0500

    Q1,2 done

---
+++ app.js
@@ -0,0 +1,54 @@
+// Question 1
+
+/*
+
+
+let x=+prompt("Enter Value 1")
+let y=+prompt("Enter Value 1")
+
+function parent(x) {
+    return function (y) {
+        return x + y
+    }
+}
+let add = parent(x);
+console.log(add(y))
+
+
+*/
+
+// ----------------------------x----
+
+// Question 2
+
+/*
+
+let arr = [1, 2, 3, 4, 22, 5, 6];
+let i = 0;
+let value = +prompt("Enter Value to search");
+
+function search(arr, i, value) {
+    if (value === arr[i]) {
+        return true;
+    } else if (i < arr.length - 1) {
+        i++;
+        return search(arr, i, value)
+    } else {
+        return false
+    }
+}
+console.log(search(arr, i, value))
+
+*/
+
+// ----------------------------x----
+
+// Question 3
+
+
+let
+
+
+// ----------------------------x----
+
+
